1f9c7194ebbb1c9c57c19ad60a7e7872939d1631
erdeno <erden.ouz@gmail.com>
2021-11-02 08:19:11 +0300

    separate frontend

--- leatherjackets/leatherjackets/settings.py
+++ leatherjackets/leatherjackets/settings.py
@@ -57,8 +57,9 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
