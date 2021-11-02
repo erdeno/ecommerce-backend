bbab25ad2d415fe43f5509fa4c204aad4d7b032b
erdeno <erden.ouz@gmail.com>
2021-11-02 09:10:49 +0300

    Created viewset for product

--- leatherjackets/product/urls.py
+++ leatherjackets/product/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from product import views
 
 urlpatterns = [
-    path('latest-products/', views.LatestProductsList.as_view())
+    path('latest-products/', views.LatestProductsList.as_view()),
+    path('products/<slug:categor_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
 ]
